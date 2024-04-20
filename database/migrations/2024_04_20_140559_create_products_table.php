0bc2eec8d68ad11399a584c1293796a6071e1e91
unknown <tonyentertain@gmail.com>
2024-04-20 15:15:12 +0100

    Adding Codebase

---
+++ database/migrations/2024_04_20_140559_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->string('product_name');
+            $table->string('product_price');
+            $table->longText('product_price')->nullable();
+            $table->boolean('in_stock')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
